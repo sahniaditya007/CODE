53947b46066d78003bc310af098cfbab6d5bc7e6
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2026-01-18 13:33:40 +0530

    Time: 8 ms (46.96%), Space: 14.5 MB (16.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,10 +4,12 @@ public:
         if (i < 0) return j + 1;
         if (j < 0) return i + 1;
 
-        if (dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        if (word1[i] == word2[j])
+        if (word1[i] == word2[j]) {
             return dp[i][j] = helper(i - 1, j - 1, word1, word2, dp);
+        }
 
         return dp[i][j] = min({
             1 + helper(i, j - 1, word1, word2, dp),
@@ -17,9 +19,10 @@ public:
     }
 
     int minDistance(string word1, string word2) {
-        int i = word1.size();
-        int j = word2.size();
-        vector<vector<int>> dp(i, vector<int>(j, -1));
-        return helper(i - 1, j - 1, word1, word2, dp);
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return helper(n - 1, m - 1, word1, word2, dp);
     }
 };
